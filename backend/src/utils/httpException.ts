d8149c0fa1ef38deab9adcda17d550c8164c21c0
Thiago Viana <trodrigo734@gmail.com>
2023-02-15 00:28:11 -0300

    Ajustes

--- backend/src/utils/httpException.ts
+++ backend/src/utils/httpException.ts
@@ -1,13 +1,9 @@
 class HttpException extends Error {
-  static handle(
-    error,
-    _req,
-    res,
-    next,
-  ) {
-    const { status, message } = error;
-    res.status(status || 500).json({ message });
-    next();
+  status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
   }
 }
 
